d23b3bf91f370df82119fcfc53a1ee248c93e193
dt1507 <darshanthakkar1569@gmail.com>
2020-11-02 22:29:38 +0530

    2 files modified

--- src/app/receipe-list/receipe-list.component.ts
+++ src/app/receipe-list/receipe-list.component.ts
@@ -1,23 +1,22 @@
-import { Component, EventEmitter,  OnInit, Output } from '@angular/core';
-import { Receipe } from '../receipes/receipe.model';
+import { Component, EventEmitter, OnInit, Output } from "@angular/core";
+import { Receipe } from "../receipes/receipe.model";
+import { RecipeListService } from "./recipe-list.service";
 
 @Component({
-  selector: 'app-receipe-list',
-  templateUrl: './receipe-list.component.html',
-  styleUrls: ['./receipe-list.component.css']
+  selector: "app-receipe-list",
+  templateUrl: "./receipe-list.component.html",
+  styleUrls: ["./receipe-list.component.css"]
 })
 export class ReceipeListComponent implements OnInit {
-  @Output()receipeWasSelected=new EventEmitter<Receipe>();
-  receipes:Receipe[]=[
-    new Receipe('Pizza','Margarita Pizza','https://images.unsplash.com/photo-1534308983496-4fabb1a015ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=755&q=80'),
-    new Receipe('Pizza','Margarita Pizza','https://images.unsplash.com/photo-1534308983496-4fabb1a015ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=755&q=80')
-  ];
-  constructor() { }
+  @Output() receipeWasSelected = new EventEmitter<Receipe>();
+  receipes: Receipe[] = [];
+  constructor(private recipeService: RecipeListService) {}
 
   ngOnInit() {
+    this.receipes = this.recipeService.getRecipe();
   }
 
-  onReceipeSelected(receipe:Receipe){
+  onReceipeSelected(receipe: Receipe) {
     this.receipeWasSelected.emit(receipe);
   }
 }
--- src/app/receipe-list/recipe-list.service.ts
+++ src/app/receipe-list/recipe-list.service.ts
@@ -1,8 +1,22 @@
-import { Injectable } from '@angular/core';
-
+import { Injectable } from "@angular/core";
+import { Receipe } from "../receipes/receipe.model";
 @Injectable()
 export class RecipeListService {
+  receipes: Receipe[] = [
+    new Receipe(
+      "Pizza",
+      "Margarita Pizza",
+      "https://images.unsplash.com/photo-1534308983496-4fabb1a015ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=755&q=80"
+    ),
+    new Receipe(
+      "Pizza",
+      "Margarita Pizza",
+      "https://images.unsplash.com/photo-1534308983496-4fabb1a015ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=755&q=80"
+    )
+  ];
+  constructor() {}
 
-  constructor() { }
-
+  getRecipe() {
+    return this.receipes.slice();
+  }
 }
